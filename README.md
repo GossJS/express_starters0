fee938617357e60b1823ec13c411534411e1e903
gossoudarev <igossoudarev@herzen.spb.ru>
2018-03-11 04:15:03 +0300

    sessions post support

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # express_starters0
 
-выполнение простой вводной лабораторной работы по сессиям, шаг 2
+выполнение простой вводной лабораторной работы по сессиям, шаг 3
 
-добавление маршрута login и шаблона для ввода логина и пароля
+добавление маршрута post login/check и обработчика post 
 
 `mkdir $(date +%Y%m%d_%H%M%S) && cd $_ && git clone -b sess2 https://github.com/GossJS/express_starters0.git . && yarn`
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const { get } = require('axios');
+const bodyParser = require('body-parser');
 
-let items; // для последующего заполнения
+let items;
 const PORT = 4321;
 const URL = 'https://kodaktor.ru/j/users';
 const app = express();
 app
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
   .get(/hello/, r => r.res.end('Hello world!'))
   .get('/login', r => r.res.render('login'))
+  .post('/login/check/', (req, res) => {
+    const { body: { login: l } } = req;
+    const user = items.find(({ login }) => login === l);
+    if (user) {
+      if (user.password === req.body.pass) {
+        res.send('Good!');
+      } else {
+        res.send('Wrong pass!');
+      }
+    } else {
+      res.send('No such user!');
+    }
+  })
   .get(/users/, async r => {
     r.res.render('list', { title: 'Список логинов', items });
   })
@@ -16,6 +32,5 @@ app
   .set('view engine', 'pug')
   .listen(process.env.PORT || PORT, async () => {
     console.log(`Старт процесса: ${process.pid}`);
-    // items = (await get(URL)).data.users;
     ({ data: { users: items } } = await get(URL)); // круглые скобки
   });
