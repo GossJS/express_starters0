efab620f50e0e8964193dc17af4aae36990b7049
gossoudarev <igossoudarev@herzen.spb.ru>
2018-04-05 12:50:08 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const { get } = require('axios');
+const User = require('./bd');
 
 const PORT = 4321;
 const URL = 'https://kodaktor.ru/j/users';
 const app = express();
 app
   .get(/hello/, r => r.res.end('Hello world!'))
-  .get(/users/, async r => {
+  .get('/aprilusers/:login', async r => {
+    const item = await User.find({ login: r.params.login });
+    console.log(item);
+    r.res.send(item[0].password);
+  })
+  .get('/aprilusers/', async r => {
+    const items = await User.find();
+    r.res.render('list', { title: 'Список логинов из БД', items });
+  })
+
+  .get('/users/', async r => {
     const { data: { users: items } } = await get(URL);
-    r.res.render('list', { title: 'Список логинов', items });
+    r.res.render('list', { title: 'Список логинов из kodaktor.ru/j/user', items });
   })
   .use(r => r.res.status(404).end('Still not here, sorry!'))
   .use((e, r, res, n) => res.status(500).end(`Error: ${e}`))
