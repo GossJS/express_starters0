b774e13e6746f653aa8e868cf9736327bb29d1c3
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-07-31 02:19:52 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const PORT = 4321;
+const app = express();
+app
+  .set('port', process.env.PORT || PORT)
+  .get(/stop/, r => r.res.end('Bye!') && process.exit(0))
+  .get('/*', r => r.res.send('Working!'))
+  .use(r => r.res.status(404).end('Still not here, sorry!'))
+  .use((e, r, res, n) => res.status(500).end(`Error: ${e}`))
+  .set('view engine', 'pug')
+  .listen(app.get('port'), () => {
+        console.log(process.pid);
+        console.log(app.get('port')); // will be like 8080 on c9
+        console.log(app.get('env')); // development  is default
+  });
